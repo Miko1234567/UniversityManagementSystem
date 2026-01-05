2b787c1dfc2744ceee8c97aede103a1236293862
Miko1234567 <Bekmurat1980kz7@gmail.com>
2026-01-06 00:43:03 +0500

    University Management System project

--- src/Professor.java
+++ src/Professor.java
@@ -1,22 +1,33 @@
-public class Professor {
-    private String name;
+import java.util.Objects;
+
+public class Professor extends Person {
     private String department;
 
     public Professor(String name, String department) {
-        this.name = name;
+        super(name); // Аталық кластың конструкторын шақыру
         this.department = department;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public String getDepartment() {
         return department;
     }
 
-    public void displayInfo() {
-        System.out.println("Professor: " + name + ", Department: " + department);
+    @Override
+    public String toString() {
+        return "Professor [Name: " + getName() + ", Department: " + department + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Professor professor = (Professor) o;
+        return Objects.equals(getName(), professor.getName()) && 
+               Objects.equals(department, professor.department);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), department);
     }
 }
-
